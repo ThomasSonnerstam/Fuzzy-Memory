1b0f43f9cf46af77afd119891cca46e82d39023a
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-12-03 09:53:00 +0100

    Added divs via JS

--- script.js
+++ script.js
@@ -32,12 +32,36 @@ const cards = [{
     }
 ];
 
-const image = document.querySelector(".card-container img");
-const containers = document.querySelectorAll(".card-container");
+const dupeCards = [...cards, ...cards]
 
-containers.forEach(container => {
-    container.addEventListener("click", () => {
-        const blocker = container.querySelector(".card-blocker");
-        blocker.classList.toggle("off");
-    })
-});
+const image = document.querySelector(".card-container img");
+const memoryCards = document.querySelectorAll(".card-blocker");
+
+
+// Helper function to prevent XSS injections
+// Creates an HTML element from string
+const stringToHTML = str => {
+    const div = document.createElement("div");
+    div.innerHTML = str;
+    return div.firstChild;
+};
+
+// Create ingredient template with a template literal
+const createCards = (name, image) => {
+    return `<div class="card-container data-name="${name}">
+    <img src="${image}" alt="">
+</div>
+    `;
+};
+
+// Render the ingredient element to the DOM
+const playboardWrapper = document.querySelector(".playboard");
+
+const generateCards = () => {
+    dupeCards.forEach(card => {
+        const element = createCards(card.name, card.image);
+        playboardWrapper.appendChild(stringToHTML(element));
+    });
+};
+
+generateCards();
