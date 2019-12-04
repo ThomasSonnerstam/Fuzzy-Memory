bb66da8242dd35fcda88c89eb00ec82c661dd0de
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-12-04 12:27:59 +0100

    Test

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 let cards = [{
-        name: "Pikachu",
-        image: "./images/pikachu.png"
-    },
-    {
-        name: "Bulbasaur",
-        image: "./images/bulbasaur.png"
-    },
-    {
-        name: "Charmander",
-        image: "./images/charmander.png"
-    },
-    {
-        name: "Eevee",
-        image: "./images/eevee.png"
-    },
-    {
-        name: "Jigglypuff",
-        image: "./images/jigglypuff.png"
-    },
-    {
-        name: "Mewtwo",
-        image: "./images/mewtwo.png"
-    },
-    {
-        name: "Snorlax",
-        image: "./images/snorlax.png"
-    },
-    {
-        name: "Squirtle",
-        image: "./images/squirtle.png"
-    }
+		name: "Pikachu",
+		image: "./images/pikachu.png"
+	},
+	{
+		name: "Bulbasaur",
+		image: "./images/bulbasaur.png"
+	},
+	{
+		name: "Charmander",
+		image: "./images/charmander.png"
+	},
+	{
+		name: "Eevee",
+		image: "./images/eevee.png"
+	},
+	{
+		name: "Jigglypuff",
+		image: "./images/jigglypuff.png"
+	},
+	{
+		name: "Mewtwo",
+		image: "./images/mewtwo.png"
+	},
+	{
+		name: "Snorlax",
+		image: "./images/snorlax.png"
+	},
+	{
+		name: "Squirtle",
+		image: "./images/squirtle.png"
+	}
 ];
 
 // START GAME
@@ -44,16 +44,16 @@ const dupeCards = [...cards, ...cards]
 // Helper function to prevent XSS injections
 // Creates an HTML element from string
 const stringToHTML = str => {
-    const div = document.createElement("div");
-    div.innerHTML = str;
-    return div.firstChild;
+	const div = document.createElement("div");
+	div.innerHTML = str;
+	return div.firstChild;
 };
 
 // Create card template with a template literal
 const image = document.querySelector(".card-container img");
 
 const createCards = (name, image) => {
-    return `<div class="card-container data-name="${name}">
+	return `<div class="card-container data-name="${name}">
                 <img src="${image}" alt="">
             </div>`;
 };
@@ -62,10 +62,10 @@ const createCards = (name, image) => {
 const playboardWrapper = document.querySelector(".playboard");
 
 const generateCards = () => {
-    dupeCards.forEach(card => {
-        const element = createCards(card.name, card.image);
-        playboardWrapper.appendChild(stringToHTML(element));
-    });
+	dupeCards.forEach(card => {
+		const element = createCards(card.name, card.image);
+		playboardWrapper.appendChild(stringToHTML(element));
+	});
 };
 
 generateCards();
@@ -79,25 +79,27 @@ const img = document.querySelector("img");
 
 
 memoryCards.forEach((memoryCard) => {
-    memoryCard.addEventListener("click", () => {
-        const img = memoryCard.querySelector("img");
-        img.classList.add("on");
-        imageArray.push(img)
-        containerArray.push(memoryCard);
-        console.log(imageArray);
-        console.log(containerArray);
+	memoryCard.addEventListener("click", () => {
+		const img = memoryCard.querySelector("img");
+		img.classList.add("on");
+		imageArray.push(img)
+		containerArray.push(memoryCard);
+		console.log(imageArray);
+		console.log(containerArray);
 
-        const checkImgArray = () => {
+		const checkImgArray = () => {
 
-            if (imageArray.length === 2) {
-                imageArray.forEach(imageArr => {
-                    setTimeout(() => {
-                        imageArr.classList.remove("on");
-                    }, 700);
-                })
-            }
-        }
-        checkImgArray();
-    })
+			if (imageArray.length === 2) {
+				imageArray.forEach(imageArr => {
+					setTimeout(() => {
+						imageArr.classList.remove("on");
+					}, 700);
+				})
+			}
+		}
+		checkImgArray();
+	})
 
 })
+
+// HEJ
